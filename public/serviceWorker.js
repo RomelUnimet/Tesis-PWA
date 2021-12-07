af2c09568ddc83f007a018fca8020e48fdc572ca
Romel Clavel <romeletoty@gmail.com>
2021-12-07 09:22:56 -0400

    Copia WorkBox de fireship

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,85 +1,52 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 //PUEDE SER NECESARIO CAMBIARLO A NPM
 
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js'); //POR ALGUNA RAZON CUANDO LO PONGO EN MI VERSION DA ERROR
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js'); //POR ALGUNA RAZON CUANDO LO PONGO EN MI VERSION DA ERROR
+if (workbox) {
+  workbox.setConfig({ debug: false });
+  // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.warn);
 
+  // workbox.routing.setDefaultHandler(workbox.strategies.networkFirst({
+  //   cacheName: 'fallback',
+  // }))
 
+  var defaultStrategy = new workbox.strategies.CacheFirst ({
+    cacheName: "fallback",
+    plugins: [
+        new workbox.expiration.ExpirationPlugin({
+            maxEntries: 128,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+            purgeOnQuotaError: true, // Opt-in to automatic cleanup
+        }),
+        new workbox.cacheableResponse.CacheableResponsePlugin({
+            statuses: [0, 200] // for opague requests
+        }),
+    ],
+});
 
-
-//POR AHORA ESTE SW LO MANTENEMOS PERO CREO QUE AHORA CAMBIA TODO A FIREBASE-MESSAGING-SW A MENOS DE QUE QUERRAMOS HACERLO DOBLE
-
-
-// Switch debug logging on/off here. Default is on in dev and off in prod.
-workbox.setConfig({debug: false});
-
-workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.CacheFirst()
+workbox.routing.setDefaultHandler(
+    (args) => {
+        if (args.event.request.method === 'GET') {
+            return defaultStrategy.handle(args); // use default strategy
+        } else {
+          return null
+        }
+        // return fetch(args.event.request);
+    }
+);
+  
+  workbox.routing.registerRoute(
+    new RegExp(/.*\.(?:js|css)/g),
+    new workbox.strategies.CacheFirst()
   );
 
-  
-  self.addEventListener('push', function(e) {
+  workbox.routing.registerRoute(
+      new RegExp(/.*\.(?:png|jpg|jpeg|svg|gif|webp)/g),
+      new workbox.strategies.CacheFirst()
+  );
+} else {
+  console.log(`No workbox on this browser 😬`);
+}
 
-      console.log('Notification Recieved')
-
-      if (Notification.permission === 'granted') {
-
-        var options = {
-          body: 'Open PWA Card Diary',
-          icon: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/95/22/9d/95229d6e-621b-ec09-6564-205b924aa380/source/200x200bb.jpg',
-          vibrate: [100, 50, 100],
-          data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-          },
-          actions: [
-            {action: 'open', title: 'Add an Entry Today',
-              icon: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/95/22/9d/95229d6e-621b-ec09-6564-205b924aa380/source/200x200bb.jpg'},
-          ]
-
-        };
-        e.waitUntil(
-
-          self.registration.showNotification('PWA Card Diary Tesis Push', options)
-          )
-        
-      }
-  });
-
-
-
-self.addEventListener('notificationclick', function(e) {
-  var notification = e.notification;
-  //var primaryKey = notification.data.primaryKey;
-  var action = e.action;
-
-  if (action === 'close') {
-    notification.close();
-    console.log('Close Notif')
-  } else {
-    clients.openWindow('https://pwacarddiarytesisrc.netlify.app/');
-    notification.close();
-    console.log('Open Notif')
-  }
-});
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDY0tuyRgv0HVBTnmoYKCSgGWTmkTlg0MA",
-  authDomain: "pwa-card-diary-tesis.firebaseapp.com",
-  projectId: "pwa-card-diary-tesis",
-  storageBucket: "pwa-card-diary-tesis.appspot.com",
-  messagingSenderId: "602718662060",
-  appId: "1:602718662060:web:ea64e957e1f8d249739e09",
-  measurementId: "G-R8FG0HMP47"
-});
-
-const messaging = firebase.messaging();
-/*
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-})
-*/
 
 
   
