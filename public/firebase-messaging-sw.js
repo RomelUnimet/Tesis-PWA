21124bc00453db1b342579577a259928eaf75efa
Romel Clavel <romeletoty@gmail.com>
2021-12-28 10:54:28 -0400

    Fixes en notificacion

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,7 +5,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js'); //POR ALGUNA RAZON CUANDO LO PONGO EN MI VERSION DA ERROR
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js'); //POR ALGUNA RAZON CUANDO LO PONGO EN MI VERSION DA ERROR
 
-
 self.addEventListener('notificationclick', function(e) {
   var notification = e.notification;
   //var primaryKey = notification.data.primaryKey;
@@ -41,7 +40,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationOptions = {
     body: 'Write down your special feelings today.',
     icon: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/95/22/9d/95229d6e-621b-ec09-6564-205b924aa380/source/200x200bb.jpg',
-    badge: './icons/manifest-icon-192.png',
+    badge: '../src/svgs/diary.svg',
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
